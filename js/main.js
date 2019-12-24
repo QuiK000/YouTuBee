502d50375ec8d2669b8024d39b7f7f693a9a68a5
QuiK <teamquiksteam@gmail.com>
2019-12-24 21:26:58 +0200

    Youtube

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const burgerMenu = document.querySelector('.header__burger-menu');
   const sideBar = document.querySelector('.main__sidebar');
+  const subscribeBtn = document.querySelector('.main__content-center-button-subscribe');
 
   burgerMenu.addEventListener('click', () => {
     sideBar.classList.toggle('unshow');
-    
   })
+
+  subscribeBtn.addEventListener('click', () => {
+    subscribeBtn.innerHTML = 'Signed';
+    subscribeBtn.style.backgroundColor = '#282828';
+  })
+
 })
