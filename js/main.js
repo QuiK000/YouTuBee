f67ec88b1998b858db0bc9548ba5eacb35e7551d
QuiK <teamquiksteam@gmail.com>
2019-12-29 12:52:42 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const burgerMenu = document.querySelector('.header__burger-menu');
   const sideBar = document.querySelector('.main__sidebar');
   const subscribeBtn = document.querySelector('.main__content-center-button-subscribe');
-  const close = document.querySelector('.auth__block-close');
-  const auth = document.querySelector('.auth');
 
   burgerMenu.addEventListener('click', () => {
     sideBar.classList.toggle('unshow');
